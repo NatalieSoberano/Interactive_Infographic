3de2cba4f69ae839ca117c78c0e7cb615f3b14a1
Natalie Soberano <nataliesoberano@Natalies-MBP-4.fc.ca>
2019-11-26 15:14:01 -0500

    working lightbox

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,29 @@
 (() => {
     //try to get the object and do stuff with it 
-    const seeMoreButton = document.querySelectorAll('.see-more'),
+    const 
+     seeMoreButton = document.querySelectorAll('.see-more'),
      lightBox = document.querySelector('.lightbox');
 
-    function buildLightBox(beerdata, el) {
-        lightBox.querySelector(".ipa-rating").textContent = `IPA Rating: ${beerdata.IpaRating}`;
-        lightBox.querySelector(".ratings").textContent = `Average Rating: ${beerdata.ratings}`;
-        lightBox.querySelector(".beer-description").textContent = beerdata.description;
+
+    function buildLightBox(princessdata, el) {
+        //lightBox.querySelector(".image").data = `images/${princessdata.image}`;
+        //lightBox.querySelector(".Pname").data = `images/${princessdata.banner}`;
+        lightBox.querySelector(".moviename").textContent = `Movie: ${princessdata.movietitle}`;
+        lightBox.querySelector(".voice").textContent = `Voice: ${princessdata.voice}`;
+        lightBox.querySelector(".age").textContent = `Age: ${princessdata.age}`;
+        lightBox.querySelector(".animal").textContent = `Animal: ${princessdata.animal}`;
+        lightBox.querySelector(".revenue").textContent = `Revenue: ${princessdata.revenue}`;
 
         // show the popover
         lightBox.classList.add('show-lightbox');
-        el.appendChild(lightBox);
+        //el.appendChild(lightBox);
     }
 
     //run the fetch API and get the DB data
     function fetchData() {
+
         let targetEl= this, 
-            url =`/svgdata/${this.dataset.target}`;
+            url =`/svgdata/${this.id}`;
 
         fetch(url)
         .then(res => res.json())
@@ -39,7 +46,7 @@
     seeMoreButton.forEach(button => button.addEventListener("click", fetchData));
 
     lightBox.querySelector('.close').addEventListener('click', function(){
-        lightBox.classList.remove('show-lb');
+        lightBox.classList.remove('show-lightbox');
     })
 
 })();
